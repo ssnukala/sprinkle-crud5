b0612d77639462181d6a50f8f05d997f82c1f8a6
ssnukala <snthelion@gmail.com>
2024-04-02 20:46:47 -0500

    Feb updates

--- app/assets/page.crudlist.js
+++ app/assets/page.crudlist.js
@@ -1,4 +1,5 @@
 global.getSlug = require("speakingurl");
+require("./crud5/js/handlebars-helpers.js");
 require("fontawesome-iconpicker/dist/js/fontawesome-iconpicker.js");
 require("fontawesome-iconpicker/dist/css/fontawesome-iconpicker.css");
 require("./crud5/js/pages/crudlist");
